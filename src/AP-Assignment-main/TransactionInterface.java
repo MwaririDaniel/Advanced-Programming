3b6a8bc10c396f1b56f856440df6bbe4d8b13d56
MwaririDaniel <158082788+MwaririDaniel@users.noreply.github.com>
2024-12-03 21:06:53 +0300

    Delete src/AP-Assignment-main directory

--- src/AP-Assignment-main/TransactionInterface.java
+++
@@ -1,9 +0,0 @@
-import java.util.Calendar;
-
-public interface TransactionInterface {
-    double getAmount(); // Get the transaction amount
-    Calendar getDate(); // Get the transaction date
-    String getTransactionID(); // Get the transaction ID
-    void printTransactionDetails(); // Print transaction details
-    void apply(BankAccount ba); // Apply transaction to the bank account
-}
